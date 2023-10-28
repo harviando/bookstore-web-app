eb63f476d6ff3ff9ae5eea42bc1b9db10289b7b0
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-10-28 12:06:11 +0700

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 8000;
+
+export const mongoDBURL =  'mongodb+srv://root:OLe5l93SnSJnxv8m@bookstore-mern.jc4jkw5.mongodb.net/books-collection?retryWrites=true&w=majority';
---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express, { response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+
+const app = express();
+
+app.use(express.json());
+
+// Route for index
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send(`Welcome to the new reality`)
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
